196502cb9fa551b24958765180ea36aaf791599e
NxtAxndr3s2 <justandres375@gmail.com>
2025-11-26 15:44:49 -0500

    Version inicial del CRUD

---
+++ nuevo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Nuevo producto</h2>
+
+<form action="store.php" method="POST">
+    Nombre: <input type="text" name="nombre"><br>
+    Precio: <input type="number" name="precio"><br>
+    <button type="submit">Guardar</button>
+</form>
+
+</body>
+</html>
---
+++ tienda.php
@@ -0,0 +1,2 @@
+<?php
+ echo "PHP esta funcionando correctamente"; 
